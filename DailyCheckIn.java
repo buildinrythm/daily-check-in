cdf80c40d8544e24945059485ac6c21ffeeffb2a
Joshua Agbor <123345261+Zaunsrevenge@users.noreply.github.com>
2025-12-17 02:40:06 +0000

    Made system ask for name and added scanner

---
+++ DailyCheckIn.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class DailyCheckIn {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        //Ask for name
+        System.out.println("What is you name?");
+    }
+}
