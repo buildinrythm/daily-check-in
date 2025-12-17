94686103361feabf52929b21ef3253e4235d40af
Joshua Agbor <123345261+Zaunsrevenge@users.noreply.github.com>
2025-12-17 03:00:33 +0000

    Added system response based on mood

--- DailyCheckIn.java
+++ DailyCheckIn.java
@@ -8,5 +8,24 @@ public class DailyCheckIn {
 
         //Ask for name
         System.out.println("What is you name?");
+        String name = scanner.nextLine();
+
+        //Ask about the day
+        System.out.println("On a scale of 1-5, how was your day?");
+        int mood = scanner.nextInt();
+
+        //Respond Based on mood
+        if(mood <= 2){
+            System.out.println(mood + "?" + " " + "Rough day champ, you'll get em next time" + " " + name);
+        } else if (mood == 3){
+        System.out.println(mood + "?" + " " + "A neutral day. We take those");
+        } else {
+            System.out.println("Nice one!" + " " + "a" + " " + mood + "?" + " " + "Glad you had a great day" + " " + name + "!");
+        }
+
+        //Final Message
+        System.out.println("Thanks for checking in!");
+
+        scanner.close();
     }
 }
